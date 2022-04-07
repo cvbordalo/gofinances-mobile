e0c8f3d669ce66ddc4e0047530f3ca0ecefb1df8
Carlos Bordalo <carlosvitor6@hotmail.com>
2022-04-07 12:01:57 -0300

    Add UserPhoto and UserInfo

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {  } from 'react-native';
 
-import { Container, Title } from './styles'
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/54056008?v=4' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Carlos</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
