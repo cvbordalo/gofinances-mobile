6a205f095b7fa048777ce23ece81805a962240b5
Carlos Bordalo <carlosvitor6@hotmail.com>
2022-04-11 16:07:21 -0300

    Finish list transactions

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {  } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { HighlightCard } from '../../components/HighlightCard';
-import { TransactionCard } from '../../components/TransactionCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import { 
   Container,
@@ -20,10 +20,15 @@ import {
   TransactionList,
 } from './styles'
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
 
-  const data = [
+  const data: DataListProps[] = [
     {
+      id: '1',
       type: 'positive',
       title: 'Desenvolvimento de site',
       amount: 'R$ 12.000,00',
@@ -35,6 +40,7 @@ export function Dashboard() {
    },
 
    {  
+      id: '2',
       type: 'negative',
       title: 'Hamburgueria Seven Kings',
       amount: 'R$ 59,00',
@@ -46,6 +52,7 @@ export function Dashboard() {
    },
 
    {
+      id: '3',
       type: 'negative',
       title: 'Aluguel do apartamento',
       amount: 'R$ 1.200,00',
@@ -81,11 +88,9 @@ export function Dashboard() {
         <Title>Listagem</Title>
           <TransactionList
             data={data}
+            keyExtractor={item => item.id}
             renderItem={({ item }) => <TransactionCard data={item} /> }
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={{
-              paddingBottom: getBottomSpace()
-            }}
+            
           />
       </Transactions>
     </Container>
