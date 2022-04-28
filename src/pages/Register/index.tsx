033a71afd29d6d464501391301af220a3fee41d8
Carlos Bordalo <carlosvitor6@hotmail.com>
2022-04-28 12:27:36 -0300

    Add AsyncStorage

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { useForm } from 'react-hook-form';
 
-import { Input } from "../../components/Form/Input";
 import { InputForm } from "../../components/Form/InputForm";
 import { Button } from "../../components/Form/Button";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
@@ -46,6 +46,8 @@ export function Register() {
     name: 'Categoria',
   });
 
+  const dataKey = '@gofinances:transactions';
+
   const {
     control,
     handleSubmit,
@@ -65,7 +67,7 @@ export function Register() {
     setCategoryModalOpen(true);
   }
 
-  function handleRegister(form: Partial<FormData>) {
+  async function handleRegister(form: Partial<FormData>) {
     if (!transactionType)
       return Alert.alert('Selecione o tipo da transação');
 
@@ -79,8 +81,23 @@ export function Register() {
       category: category.key
     }
 
-    console.log(data)
+    try {
+      await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Não foi possível salvar seus dados');
+    }
   }
+
+  useEffect(() => {
+    async function loadData() {
+      const data = await AsyncStorage.getItem(dataKey);
+      console.log(JSON.parse(data!));
+    }
+
+    loadData();
+  }, []);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
